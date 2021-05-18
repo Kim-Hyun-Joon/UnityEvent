bcdb4c3d657679d1e4400295d0742160a1ed3909
김 현준 <jkajks123456@einssnc.com>
2021-05-18 11:09:00 +0900

    Invoke를 통한 유니티이벤트 다루기

---
+++ Assets/Scripts/AchivementSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchivementSystem : MonoBehaviour
+{
+    public Text achivementText;
+
+    public void UnLockAchivement(string title) {
+        Debug.Log("도전 과제 해제 " + title);
+        achivementText.text = "도전과제 해제" + title;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    /// 이처럼 하드하게 알고 있어야 얘네들의 기능을 발동시킬 수 있다
+    //public UIManager UIManager;
+    //public AchivementSystem achivementSystem;
+    //public GameManager gameManager;
+    /// 플레이어의 죽음과 관련된 스크립트가 늘어날수록
+    /// 계속해서 선언을 통해 사용하는 것은 지저분한 방법이다
+    /// 
+
+    public UnityEvent onPlayerDead;
+
+    private void Dead() {
+
+        //UIManager.OnPlayerDead();
+        //achivementSystem.UnLockAchivement("뉴턴의 법칙");
+        //gameManager.OnPlayerDead();
+
+        onPlayerDead.Invoke();
+
+        Debug.Log("죽었다");
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other) {
+        Dead();
+    }
+}
